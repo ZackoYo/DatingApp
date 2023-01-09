9b1f78c7d15160fa70c37b3678ac9ff6e155aeb0
Yordanov Zahari <yordanovz@aforbes.com>
2023-01-09 17:45:01 +0200

    Fix CORS policy issue

--- API/Program.cs
+++ API/Program.cs
@@ -15,9 +15,14 @@ builder.Services.AddDbContext<DataContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+app.UseCors(corsBuilder => corsBuilder.AllowAnyHeader().WithOrigins("http://localhost:4200"));
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
