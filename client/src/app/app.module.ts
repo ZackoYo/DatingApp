3a7c8d2056852bf3124922a19545a34cf77b5960
Yordanov Zahari <yordanovz@aforbes.com>
2023-03-11 15:01:54 +0200

    Handle not found

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -16,6 +16,8 @@ import { MessagesComponent } from './messages/messages.component';
 import { SharedModule } from './_modules/shared.module';
 import { TestErrorComponent } from './errors/test-error/test-error.component';
 import { ErrorInterceptor } from './_interceptors/error.interceptor';
+import { ServerErrorComponent } from './errors/server-error/server-error.component';
+import { NotFoundComponent } from './errors/not-found/not-found.component';
 
 @NgModule({
   declarations: [
@@ -28,6 +30,8 @@ import { ErrorInterceptor } from './_interceptors/error.interceptor';
     ListsComponent,
     MessagesComponent,
     TestErrorComponent,
+    ServerErrorComponent,
+    NotFoundComponent,
   ],
   imports: [
     BrowserAnimationsModule,
