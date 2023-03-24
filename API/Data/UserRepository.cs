65aef5d904590161f9195db84b24d1e56b2e1ebb
Yordanov Zahari <yordanovz@aforbes.com>
2023-03-24 20:42:28 +0200

    Use pagination classes

--- API/Data/UserRepository.cs
+++ API/Data/UserRepository.cs
@@ -1,8 +1,10 @@
 using API.DTOs;
 using API.Entities;
+using API.Helpers;
 using API.Interfaces;
 using AutoMapper;
 using AutoMapper.QueryableExtensions;
+using Microsoft.AspNetCore.Mvc.Diagnostics;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Data
@@ -26,11 +28,13 @@ namespace API.Data
                 .SingleOrDefaultAsync();
         }
 
-        public async Task<IEnumerable<MemberDto>> GetMembersAsync()
+        public async Task<PagedList<MemberDto>> GetMembersAsync(UserParams userParams)
         {
-            return await _context.Users
+            var query = _context.Users
                 .ProjectTo<MemberDto>(_mapper.ConfigurationProvider)
-                .ToListAsync();
+                .AsNoTracking();
+
+            return await PagedList<MemberDto>.CreateAsync(query, userParams.PageNumber, userParams.PageSize);   
         }
         public async Task<AppUser> GetUserByIdAsync(int id)
         {
