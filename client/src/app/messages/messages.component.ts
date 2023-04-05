959c50425f7f1c2255c8c01d0b5c075f81692677
Yordanov Zahari <yordanovz@aforbes.com>
2023-04-05 23:39:14 +0300

    Add messages to client

--- client/src/app/messages/messages.component.ts
+++ client/src/app/messages/messages.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Message } from '../_models/message';
+import { Pagination } from '../_models/pagination';
+import { MessagesService } from '../_services/messages.service';
 
 @Component({
   selector: 'app-messages',
@@ -6,10 +9,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./messages.component.css']
 })
 export class MessagesComponent implements OnInit {
+  messages?: Message[];
+  pagination?: Pagination;
+  container = 'Outbox';
+  pageNumber = 1;
+  pageSize = 5;
+  loading = false;
 
-  constructor() { }
+  constructor(private messagesService: MessagesService) { }
 
   ngOnInit(): void {
+    this.loadMessages();
+  }
+
+  loadMessages() {
+    this.loading = true;
+    this.messagesService.getMessages(this.pageNumber, this.pageSize, this.container).subscribe({
+      next: response => {
+        this.messages = response.result;
+        this.pagination = response.pagination;
+        this.loading = false;
+      }
+    })
+  }
+
+  pageChanged(event: any) {
+    if (this.pageNumber !== event.page) {
+      this.pageNumber = event.page;
+      this.loadMessages();
+    }
   }
 
 }
