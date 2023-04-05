7cadf7ebae3d9b0e201c8afd634b8ae378581a08
Yordanov Zahari <yordanovz@aforbes.com>
2023-04-05 09:39:05 +0300

    Add the message thread

--- API/Interfaces/IMessageRepository.cs
+++ API/Interfaces/IMessageRepository.cs
@@ -10,7 +10,7 @@ namespace API.Interfaces
         void DeleteMessage(Message message);
         Task<Message> GetMessage(int id);
         Task<PagedList<MessageDto>> GetMessagesForUser(MessageParams messageParams);
-        Task<IEnumerable<MessageDto>> GetMessageThread(string currentUserId, string recipientUserId); 
+        Task<IEnumerable<MessageDto>> GetMessageThread(string CurrentUsername, string RecipientUsername); 
         Task<bool> SaveAllAsync();
     }
 }
