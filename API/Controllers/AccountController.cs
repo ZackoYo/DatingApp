993c5c18b15f2ff7b7b9091e8c12ca06598ea778
Yordanov Zahari <yordanovz@aforbes.com>
2023-04-28 16:16:18 +0300

    Set up Entities

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -31,11 +31,7 @@ namespace API.Controllers
 
             var user = _autoMapper.Map<AppUser>(registerDto);
 
-            using var hmac = new HMACSHA512();
-
             user.UserName = registerDto.Username.ToLower();
-            user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password));
-            user.PasswordSalt = hmac.Key;
 
             _context.Users.Add(user);
             await _context.SaveChangesAsync();
@@ -57,15 +53,6 @@ namespace API.Controllers
             .SingleOrDefaultAsync(u => u.UserName == loginDto.Username);
 
             if(user == null) return Unauthorized("invalid username");
-
-            using var hmac = new HMACSHA512(user.PasswordSalt);
-            
-            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
-
-            for (int i = 0; i < computedHash.Length; i++)
-            {
-                if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("invalid password");
-            }
 
             return new UserDto
             {
