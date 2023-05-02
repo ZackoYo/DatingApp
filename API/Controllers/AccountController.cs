843ed464a61212657aeb8ff1645e55f091bfbbc0
Yordanov Zahari <yordanovz@aforbes.com>
2023-05-02 20:21:29 +0300

    Add roles in JWT and use then for authorization

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -45,7 +45,7 @@ namespace API.Controllers
             return new UserDto
             {
                 Username = user.UserName,
-                Token = _tokenService.CreateToken(user),
+                Token = await _tokenService.CreateToken(user),
                 KnownAs = user.KnownAs,
                 Gender = user.Gender
             };
@@ -61,13 +61,13 @@ namespace API.Controllers
             if(user == null) return Unauthorized("invalid username");
 
             var result = await _userManager.CheckPasswordAsync(user, loginDto.Password);
-            
+
             if (!result) return Unauthorized("Invalid password");
 
             return new UserDto
             {
                 Username = user.UserName,
-                Token = _tokenService.CreateToken(user),
+                Token = await _tokenService.CreateToken(user),
                 PhotoUrl = user.Photos.FirstOrDefault(ph => ph.IsMain)?.Url,
                 KnownAs = user.KnownAs,
                 Gender = user.Gender
