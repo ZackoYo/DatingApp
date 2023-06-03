e070f98d9bf72f07d12a04c651a8a37a77a09cda
Yordanov Zahari <yordanovz@aforbes.com>
2023-06-03 19:14:02 +0300

    Tidying up the member message component

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -22,6 +22,7 @@ export class NavComponent implements OnInit {
     this.accountService.login(this.model).subscribe({
       next: _ => {
         this.router.navigateByUrl('/members');
+        this.model = {};
       }
     })
   }
